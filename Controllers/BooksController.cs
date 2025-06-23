326a18874b1b3896741c83d74d61fb517b96c518
Jakkmalm <jacobmalmberg@hotmail.com>
2025-06-23 08:04:57 +0200

    Upd CRUD

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using BookQuotesApi.Models;
 using System.Linq;
+using System.Security.Claims;
 
 
 
@@ -16,44 +17,69 @@ namespace BookQuotesApi.Controllers
         public BooksController(AppDbContext db) => _db = db;
 
         [HttpGet]
-        public IActionResult GetAll() =>
-            Ok(_db.Books.ToList());
+        public IActionResult GetAll()
+        {
+            var userId = GetUserId();
+            var books = _db.Books.Where(b => b.AppUserId == userId).ToList();
+            return Ok(books);
+        }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            var book = _db.Books.Find(id);
+            var userId = GetUserId();
+            var book = _db.Books.FirstOrDefault(b => b.Id == id && b.AppUserId == userId);
             return book == null ? NotFound() : Ok(book);
         }
 
         [HttpPost]
         public IActionResult Create(Book book)
         {
+            var userId = GetUserId();
+            book.AppUserId = userId;
+
             _db.Books.Add(book);
             _db.SaveChanges();
+
             return CreatedAtAction(nameof(GetById), new { id = book.Id }, book);
         }
 
         [HttpPut("{id}")]
         public IActionResult Update(int id, Book updated)
         {
-            var book = _db.Books.Find(id);
+            var userId = GetUserId();
+            var book = _db.Books.FirstOrDefault(b => b.Id == id && b.AppUserId == userId);
             if (book == null) return NotFound();
+
             book.Title = updated.Title;
             book.Author = updated.Author;
             book.Published = updated.Published;
             _db.SaveChanges();
+
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var book = _db.Books.Find(id);
+            var userId = GetUserId();
+            var book = _db.Books.FirstOrDefault(b => b.Id == id && b.AppUserId == userId);
             if (book == null) return NotFound();
+
             _db.Books.Remove(book);
             _db.SaveChanges();
+
             return NoContent();
+        }
+
+        private int GetUserId()
+        {
+            var userIdStr = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userIdStr))
+            {
+                throw new UnauthorizedAccessException("Token saknar NameIdentifier");
+            }
+            return int.Parse(userIdStr);
         }
     }
 }
