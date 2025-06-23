1d21dd29003b7ee8138c5dda4090cb8a52a2165d
Jakkmalm <jacobmalmberg@hotmail.com>
2025-06-23 07:44:51 +0200

    FK AppUser Books

--- Migrations/20250623054302_LinkBooksToAppUser.Designer.cs
+++ Migrations/20250623054302_LinkBooksToAppUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookQuotesApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BookQuotesApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250623054302_LinkBooksToAppUser")]
+    partial class LinkBooksToAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250623054302_LinkBooksToAppUser.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookQuotesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkBooksToAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppUserId",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AppUserId",
+                table: "Books",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Users_AppUserId",
+                table: "Books",
+                column: "AppUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Users_AppUserId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AppUserId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Books");
+        }
+    }
+}
